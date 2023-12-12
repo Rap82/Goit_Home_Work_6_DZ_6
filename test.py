965776a6347d64acda02ed3e82225809877ea99e
Rap82 <148592319+Rap82@users.noreply.github.com>
2023-12-12 15:12:54 +0200

    Task 3

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
-from pathlib import Path    # Підключаємо функцію *Path для пошуку шляху до  файлу для читання з бібліотеки *pathlib
 
-def write_employees_to_file( employee_list, path) :
+# ++++++++++++++++++++++++++++++++ Чисті коди для автоперевірки без широких коментарів. +++++++++++++++++++++
 
-    print (path)
+# from pathlib import Path    # Підключаємо функцію *Path для пошуку шляху до  файлу для читання з бібліотеки *pathlib
+
+# def write_employees_to_file( employee_list, path) :
+
+#     print (path)
    
-    try: 
-        fh = open (path , 'w')
+#     try: 
+#         fh = open (path , 'w')
         
-        for employee_idex in employee_list :
-            for employee in employee_idex:
+#         for employee_idex in employee_list :
+#             for employee in employee_idex:
             
-                new_employee = employee + '\n'
-                print(new_employee)
-                fh.write (new_employee )
+#                 new_employee = employee + '\n'
+#                 print(new_employee)
+#                 fh.write (new_employee )
        
       
 
@@ -20,15 +23,48 @@ def write_employees_to_file( employee_list, path) :
         
 
 
-    finally:        # який би збій не був в середені коду закриває відкритий файл в кінці.
+#     finally:        # який би збій не був в середені коду закриває відкритий файл в кінці.
 
-        fh.close()  # Вудована функція закритя файлу який був відкритий вище. *fh.close(), Де fh - зміна в яку передали вікритий файл , *.close() метод закриття файлу.
+#         fh.close()  # Вудована функція закритя файлу який був відкритий вище. *fh.close(), Де fh - зміна в яку передали вікритий файл , *.close() метод закриття файлу.
 
 
+
+# path = Path ( "employee_list_Zavdania_2.txt" )
+
+# employee_list = [ ["Robert Stivenson,28", "Alex Denver,30", "Drake Mikelsson,19"] ,["Ro Stiv,48", "Alx Dver,50", "Dke Mison,49"]]
+
+
+# write_employees_to_file(employee_list, path) 
+
+
+# ***************************************************************************************************************************
+
+
+from pathlib import Path   
+
+import re  
+def read_employees_from_file (path) :
+
+    try:   
+
+        fh = open (path , "r")  
+        lines = fh.readlines() 
+       
+        patern = "\n"   
+        replace = "" 
+
+        list_employees_from_file = []
+
+        for line in lines : 
+            employee_from_file = re.sub (patern, replace , line ) 
+
+            list_employees_from_file.append(employee_from_file)
+    
+        return  list_employees_from_file 
+
+    finally:        
+        fh.close() 
 
 path = Path ( "employee_list_Zavdania_2.txt" )
 
-employee_list = [ ["Robert Stivenson,28", "Alex Denver,30", "Drake Mikelsson,19"] ,["Ro Stiv,48", "Alx Dver,50", "Dke Mison,49"]]
-
-
-write_employees_to_file(employee_list, path) 
+print(read_employees_from_file (path))
