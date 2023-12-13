ee91890616a63d9b89c9485026ee68dc7afc3172
Rap82 <148592319+Rap82@users.noreply.github.com>
2023-12-14 01:03:45 +0200

    Task 5

--- test.py
+++ test.py
@@ -40,31 +40,67 @@
 # ***************************************************************************************************************************
 
 
-from pathlib import Path   
+# from pathlib import Path   
 
-import re  
-def read_employees_from_file (path) :
+# import re  
+# def read_employees_from_file (path) :
 
-    try:   
+#     try:   
 
-        fh = open (path , "r")  
-        lines = fh.readlines() 
+#         fh = open (path , "r")  
+#         lines = fh.readlines() 
        
-        patern = "\n"   
-        replace = "" 
+#         patern = "\n"   
+#         replace = "" 
 
-        list_employees_from_file = []
+#         list_employees_from_file = []
 
-        for line in lines : 
-            employee_from_file = re.sub (patern, replace , line ) 
+#         for line in lines : 
+#             employee_from_file = re.sub (patern, replace , line ) 
 
-            list_employees_from_file.append(employee_from_file)
+#             list_employees_from_file.append(employee_from_file)
     
-        return  list_employees_from_file 
+#         return  list_employees_from_file 
 
-    finally:        
-        fh.close() 
+#     finally:        
+#         fh.close() 
 
-path = Path ( "employee_list_Zavdania_2.txt" )
+# path = Path ( "employee_list_Zavdania_2.txt" )
 
-print(read_employees_from_file (path))
+# print(read_employees_from_file (path))
+
+
+
+
+
+
+from pathlib import Path
+import re
+
+def get_cats_info(path):
+ 
+ 
+ cats_info_list_all = []
+ Key_for_dict = ["id", "name", "age"]
+ cat_info_dict_up = {}
+
+ with open (path , "r") as fh :
+        
+      #print (path)
+      cats_info = fh.readlines()
+      #print (cats_info)
+      patern = "\n"   
+      replace = ""
+      for cat_inf in cats_info :
+         cat_info =  re.sub (patern, replace , cat_inf ) 
+         #print(cat_info)
+         cat_info_list = re.split (",", cat_info)
+         #print(cat_info_list)
+         cat_info_dict_up = dict(zip(Key_for_dict,cat_info_list))      
+         
+            
+         print (cat_info_dict_up)
+         
+         cats_info_list_all.append(cat_info_dict_up)
+      #print(f"cats_info_list_all = {cats_info_list_all}\n")
+ return cats_info_list_all
